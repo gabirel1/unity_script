55f60e7687b008e9d028bd8c520dab58e40e25c1
Gabriel <40573272+gabirel1@users.noreply.github.com>
2020-02-03 11:38:42 +0100

    adding some script for future unity projects

---
+++ MainMenu.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour {
+
+  public void Play() 
+  {
+    SceneManager.LoadScene("GameScene");
+  }
+
+  public void Play2()
+    {
+        SceneManager.LoadScene("RulesCredits");
+    }
+
+  public void Quit()
+  {
+    Application.Quit();
+  }
+}
---
+++ PauseMenuScript.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PauseMenuScript : MonoBehaviour {
+
+public GameObject ui;
+
+	void Update () {
+		if (Input.GetKeyDown(key: KeyCode.Escape)){ // Si la touche (Echap) est appuyés, on apelle la fonction Toggle()
+			Toggle();
+		} 
+	} 
+	void Toggle() 
+	{
+		ui.SetActive(!ui.activeSelf);
+		if (ui.activeSelf)
+		{
+			Time.timeScale = 0f; //On définie le temps sur 0 (on coupe l'écoulement du temps)
+		}
+		else
+		{
+			Time.timeScale = 1f; //On définie le temps sur 1 (on remet l'écoulement du temps)
+		}
+	}
+	public void Return() 
+	{
+		SceneManager.LoadScene("MainMenuScene"); // On appelle la fonction SceneManager, et on lui demande de charger la scène MainMenuScene
+	}
+	public void Quit()
+	{
+		Application.Quit();
+	}	
+}
